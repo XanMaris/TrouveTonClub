907b7595c8ea536ba0a52c50460556beb9337b26
NicolasGuruphat <nicolas.guruphat@gmail.com>
2022-11-19 19:25:13 +0100

    Pop up first version

---
+++ Frontend/TrouveTonClub/src/components/PopUpClub.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="main-wrapper">
+        <button id="close" @click="$emit('switchDisplay')">X</button>
+        <div id="information-wrapper">
+            <p>
+                nom
+            </p>
+            <p>
+                localisation
+            </p>
+            <p>
+                ...
+            </p>
+            <p>
+                ...
+            </p>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+
+#main-wrapper {
+    position: absolute;
+    top: 5%;
+    left: 5%;
+    z-index: 1000;
+    background: rgba(141, 134, 134, 0.925);
+    width: 90%;
+    height: 90%;
+    border-radius: 20px;
+}
+#information-wrapper{
+    padding-left:5%;
+    padding-top:2.5%;   
+    color: black;   
+    font-size: 150%;
+}
+#close{
+    position: absolute;
+    right:1.5%;
+    top:2.5%
+}
+</style>
--- Frontend/TrouveTonClub/src/views/Search.vue
+++ Frontend/TrouveTonClub/src/views/Search.vue
@@ -1,21 +1,31 @@
+<template>
+  <div class="d-flex flex-row w-75 justify-content-center">
+    <SearchPanal></SearchPanal>
+    <Map :latitude = 45.764043 :longtitude= 4.835659>
+    </Map>
+  </div>
+  <PopUpClub v-if="displayPopUp" @switchDisplay="displayPopUp=!displayPopUp" />
+</template>
+
 <script lang="ts">
 import Map from "../components/Map.vue";
 import SearchPanal from "../components/SearchPanal.vue";
-
+import PopUpClub from "@/components/PopUpClub.vue";
 export default {
   components: {
     Map,
-    SearchPanal
+    SearchPanal,
+    PopUpClub
+  },
+  data() {
+    return {
+      displayPopUp: true,
+    }
   }
   
 }
 </script>
 
-<template>
-    <div class="d-flex flex-row w-75 justify-content-center">
-      <SearchPanal></SearchPanal>
-      <Map :latitude = 45.764043 :longtitude= 4.835659></Map>
-    </div>
-</template>
+
 
 
