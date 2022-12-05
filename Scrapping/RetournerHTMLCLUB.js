ab18076d0c012e17936647de6f428bf0ccc1f540
Xan <xanmaris.pro@gmail.com>
2022-12-05 07:53:48 +0100

    Mise en place sous arbre

--- Scrapping/RetournerHTMLCLUB.js
+++ Scrapping/RetournerHTMLCLUB.js
@@ -25,11 +25,41 @@ page.open(url, function() {
             u++;
         }
         return { 'liste': maliste, 'lien': tablien }
+
     });
+
+
+
     listeComite = tab.liste
     listeLien = tab.lien
-
-
-    console.log(listeComite);
     phantom.exit();
 });
+
+for (let i = 0; i < listeLien.length; i++) {
+    page.open(listeLien[i], function() {
+        tab = page.evaluate(function() {
+            liste = document.documentElement.getElementsByClassName('liste')[0];
+            var maliste = [];
+            var tablien = [];
+            var u = 0;
+
+            for (var i = 1, row; row = table.rows[i]; i++) {
+                //rows would be accessed using the "row" variable assigned in the for loop
+
+                maliste[u] = table.rows[i].cells[1].innerText;
+                if (table.rows[i].cells[1].getElementsByTagName("a")[0].href == undefined) {
+                    tablien[u] = "";
+                } else {
+                    tablien[u] = table.rows[i].cells[1].getElementsByTagName("a")[0].href;
+                }
+                u++;
+            }
+            return { 'liste': maliste, 'lien': tablien }
+        });
+    });
+
+
+
+    console.log(tab.lien);
+
+}
