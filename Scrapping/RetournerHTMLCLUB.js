f12a25cf5b9ce95a68a2ee2fd4699d9afe93d5c3
unknown <Xan@EPULAPP2214.univ-lyon1.fr>
2022-12-05 14:21:41 +0100

    Finalisation deuxieme branche

--- Scrapping/RetournerHTMLCLUB.js
+++ Scrapping/RetournerHTMLCLUB.js
@@ -1,16 +1,16 @@
 var listeComite = [];
 var listeLien = [];
 var tabClub = [];
-function PremierProg()
-{
+function PremierProg() {
     var page = require('webpage').create();
     var url = 'https://resultats.ffbb.com/comites.html';
     var tableau_Region = [];
     tab = [];
-    
-    page.open(url, function() {
 
-        tab = page.evaluate(function() {
+
+    page.open(url, function () {
+
+        tab = page.evaluate(function () {
 
             table = document.getElementsByClassName('liste')[0];
             var maliste = [];
@@ -28,7 +28,7 @@ function PremierProg()
                 }
                 u++;
             }
-            return { 'liste': maliste, 'lien': tablien } 
+            return { 'liste': maliste, 'lien': tablien }
 
         });
         listeComite = tab.liste;
@@ -39,39 +39,40 @@ function PremierProg()
 }
 
 
-function deuxiemeProg(url)
-{
-        var page2 = require('webpage').create();
-        page2.open(url, function() {
-            tabClub += page2.evaluate(function() {
-                
-               table =  document.findById("idIframeOrganismeFils").contentDocument.getElementsByClassName('liste')[0];
-                var maliste = [];
-                var tablien = [];
-                var u = 0;
+function deuxiemeProg(url) {
+    var page2 = require('webpage').create();
+    tab = [];
 
-                for (var i = 1, row; row = table.rows[i]; i++) {
-                    //rows would be accessed using the "row" variable assigned in the for loop
+    page2.open(url, function () {
 
-                    maliste[u] = table.rows[i].cells[2].innerText;
-                    if (table.rows[i].cells[2].getElementsByTagName("a")[0].href == undefined) {
-                        tablien[u] = "";
-                    } else {
-                        tablien[u] = table.rows[i].cells[2].getElementsByTagName("a")[0].href;
-                    }
-                    u++;
+        tabClub = page2.evaluate(function () {
+
+            table = document.getElementById("idIframeOrganismeFils").contentDocument.getElementsByClassName('liste')[0];
+            var maliste = [];
+            var tablien = [];
+            var u = 0;
+
+            for (var i = 1, row; row = table.rows[i]; i++) {
+                //rows would be accessed using the "row" variable assigned in the for loop
+
+                maliste[u] = table.rows[i].cells[1].innerText;
+                if (table.rows[i].cells[1].getElementsByTagName("a")[0].href == undefined) {
+                    tablien[u] = "";
+                } else {
+                    tablien[u] = table.rows[i].cells[1].getElementsByTagName("a")[0].href;
                 }
-                return { 'liste': maliste, 'lien': tablien }
-            });
-            console.log(tabClub.lien);
-            phantom.exit();
+                u++;
+            }
+            return { 'liste': maliste, 'lien': tablien }
+
         });
+        console.log(tabClub.maliste);
+        phantom.exit();
+    });
 }
 
-
-//PremierProg();
-tabOui = ["https://resultats.ffbb.com/organisation/7eb.html","https://resultats.ffbb.com/organisation/7f3.html","https://resultats.ffbb.com/organisation/7f2.html"];
-for (i=1;i<tabOui.length;i++)
+PremierProg();
+for (i=0;i<listeLien.length;i++)
 {
-    deuxiemeProg(tabOui[i]);
+    deuxiemeProg(listeLien);
 }
