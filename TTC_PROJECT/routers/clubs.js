cb979d945a1332a91d40c3202fdd691b20336146
NicolasGuruphat <nicolas.guruphat@gmail.com>
2022-12-16 10:03:02 +0100

    add route for get with parameters

--- TTC_PROJECT/routers/clubs.js
+++ TTC_PROJECT/routers/clubs.js
@@ -32,4 +32,33 @@ router.post('/', (req, res)=>{
       });
 });
 
+router.get('/:id', (req, res)=>{
+    const dbConnect = dbo.getDb();
+    dbConnect
+    .collection("clubs")
+    .findOne({id:req.params.id}, function (err, result) {
+      if (err) {
+        res.status(400).send("Error fetching listings!");
+     } else {
+        res.json(result);
+      }
+    });
+});
+
+// get clubs with parameters
+
+//=acos(sin(lat1)*sin(lat2)+cos(lat1)*cos(lat2)*cos(lon2-lon1))*6371    
+router.get('/search/:param', (req, res)=>{
+    const dbConnect = dbo.getDb();
+    dbConnect
+    .collection("clubs")
+    .find( { position: {$lte: [acos(sin(req.params.position.lat)*sin(position.lat)+cos(req.params.position.lat)*cos(position.lat)*cos(position.lon-req.params.position.lon))*6371,10] } } )
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send("Error fetching listings!");
+     } else {
+        res.json(result);
+      }
+    });
+});
 module.exports = router;
